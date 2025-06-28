ec9d8851963abf5e5ae8443e1495590254914417
Yongick Cho <yongickc@princeton.edu>
2025-06-28 15:44:30 -0400

    Update relax.py

--- src/relax.py
+++ src/relax.py
@@ -13,8 +13,8 @@ def relax(kIB,kBI,lattice,Ly,Lx,e,df,µdr,D,eta,rng):
 
     for count in range(100*(lattice != 0).sum()):
         cumul = mat_rate.sum()
-        rnd = rng.random()*cumul
-        v = np.searchsorted(mat_rate.cumsum(),rnd,side="right")
+        rn = rng.random() * cumul
+        v = np.searchsorted(mat_rate.cumsum(),rn,side="right")
         y = v // Lx
         x = v % Lx
 
@@ -25,21 +25,21 @@ def relax(kIB,kBI,lattice,Ly,Lx,e,df,µdr,D,eta,rng):
         nB = mat_nB[y,x]
         nBI = mat_nBI[y,x]
 
-        rnd = rng.random() * mat_rate[y,x]
+        rn = rng.random() * mat_rate[y,x]
         # Diffusion
-        if (molecule_type == 1 and rnd < D*np.exp(e*nB)*(4-nBI)/4) or\
-                (molecule_type == -1 and rnd < D*(4-nBI)/4):
+        if (molecule_type == 1 and rn < D*np.exp(e*nB)*(4-nBI)/4) or\
+                (molecule_type == -1 and rn < D*(4-nBI)/4):
             while True:
-                rnd1 = rng.random()
-                if rnd1 < 0.25:
+                rn = rng.random()
+                if rn < 0.25:
                     if lattice[(y+1)%Ly,x] == 0:
                         update_y = 1
                         break
-                elif rnd1 < 0.5:
+                elif rn < 0.5:
                     if lattice[y,(x+1)%Lx] == 0:
                         update_x = 1
                         break
-                elif rnd1 < 0.75:
+                elif rn < 0.75:
                     if lattice[(y-1)%Ly,x] == 0:
                         update_y = -1
                         break
